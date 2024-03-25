e28732bcbfee301a2e286117ebb2a386ab1bbcb6
DatIsLeon <Lorenzopolak@hotmail.com>
2024-03-25 16:00:48 +0100

    Command prompt starts for UEM barebones and full version

---
+++ [BAREBONES] ULTIMATE EXPERIENCE MOD.bat
@@ -0,0 +1,10 @@
+@ECHO ON
+::Kills the Black Ops III game and T7 Patch if they are still running
+taskkill/im BlackOps3.exe /t /f
+taskkill/im t7patch_2.02.exe /t /f
+START	%INSERT PATH TO T7 PATCH HERE%\t7patch_2.02.exe
+::Example C:\Users\Username\Bureaublad\t7patch_2.02.exe
+cd /d "INSERT PATH TO BLACK OPS III LAUNCHER HERE\"
+::Example: "C:\SteamLibrary\steamapps\common\Call of Duty Black Ops III\"
+START	BlackOps3.exe +set fs_game 3064538158
+::+set fs_game will open black ops III with Barebones Ultimate Experience Mod loaded in
